6c5b0331991b7ea72213d0c079a2afd950f503d9
briankamanga <73723985+briankamanga@users.noreply.github.com>
2026-01-09 01:34:29 +0000

    Just unzipped the project - this would be treated as the main project where PRs would be made Initial commit of Briankamangagroup Information Management System Backend.

---
+++ briankamangagroup-information-management-system-backend/src/main/java/com/briankamangagroup/briankamangagroup_information_management_system/BriankamangagroupInformationManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.briankamangagroup.briankamangagroup_information_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriankamangagroupInformationManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BriankamangagroupInformationManagementSystemBackendApplication.class, args);
+	}
+
+}
---
+++ briankamangagroup-information-management-system-backend/src/test/java/com/briankamangagroup/briankamangagroup_information_management_system/BriankamangagroupInformationManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briankamangagroup.briankamangagroup_information_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriankamangagroupInformationManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
